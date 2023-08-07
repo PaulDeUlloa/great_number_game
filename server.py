98f0488169c1946cfcd6dbf5d26b64556898c3d3
Paul De Ulloa <pauld143@gmail.com>
2023-08-06 18:58:11 -0700

    complete with bootstrap added

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, session, redirect
+import random
 
 app = Flask(__name__)
 app.secret_key = "Gr3at_numb3r_key736"
@@ -6,16 +7,21 @@ app.secret_key = "Gr3at_numb3r_key736"
 
 @app.route("/")
 def index():
-    import random
-
-    random.randint(1, 100)
+    if "num" not in session:
+        session["num"] = random.randint(1, 100)
     return render_template("index.html")
 
 
 @app.route("/guess", methods=["POST"])
-def grab_num():
-    print("User Guess")
-    print(request.form["user_num_input"])
+def guess():
+    session["guess"] = int(request.form["guess"])
+    print(int(request.form["guess"]))
+    return redirect("/")
+
+
+@app.route("/reset")
+def reset():
+    session.clear()
     return redirect("/")
 
 
