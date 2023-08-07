7b30e44408e13ab2ea59b484ff28831a3da31181
Paul De Ulloa <pauld143@gmail.com>
2023-08-06 18:21:49 -0700

    HTML and server updates

--- server.py
+++ server.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect
 
 app = Flask(__name__)
+app.secret_key = "Gr3at_numb3r_key736"
 
 
 @app.route("/")
 def index():
+    import random
+
+    random.randint(1, 100)
     return render_template("index.html")
+
+
+@app.route("/guess", methods=["POST"])
+def grab_num():
+    print("User Guess")
+    print(request.form["user_num_input"])
+    return redirect("/")
 
 
 if __name__ == "__main__":
